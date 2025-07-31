61a2cd14788b7ce93abf8dae0f8b9e5ab04ed5f4
Lucas Corrêa <l.scorrea.01@gmail.com>
2025-07-30 23:58:44 -0300

    Server new feats #3

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -11,10 +11,10 @@ await seed(db, schema).refine(f => {
             columns: {
                 name: f.companyName(),
                 description: f.loremIpsum(),
-            },
-            with: {
-                questions: 5,
-            }
+            },  
+        },
+        questions: {
+            count: 20,
         }
     })
 })
--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,8 @@ import {
 import { fastifyCors} from '@fastify/cors'
 import { env } from './env.ts';
 import { getRoomsRoute } from './http/routes/get-rooms.ts';
+import { createRoomsRoute } from './http/routes/create-room.ts';
+import { getRoomsQuestions } from './http/routes/get-room-question.ts';
 
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -23,5 +25,7 @@ app.get('/health', () => {
 })
 
 app.register(getRoomsRoute)
+app.register(getRoomsQuestions)
+app.register(createRoomsRoute)
 
 app.listen({ port: env.PORT })
