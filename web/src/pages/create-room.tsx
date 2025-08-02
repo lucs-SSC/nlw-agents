af13595a4a431cd3e8590bf82bbb4aded874c87d
Lucas Corrêa <l.scorrea.01@gmail.com>
2025-08-01 23:43:09 -0300

    Web new feats #1

--- web/src/pages/create-room.tsx
+++ web/src/pages/create-room.tsx
@@ -1,36 +1,29 @@
 import { useQuery } from "@tanstack/react-query"
 import { Link } from "react-router-dom"
 
-type GetRoomsAPIResponse = Array<{
-    id: string,
-    name: string,
-}>
+// type GetRoomsAPIResponse = Array<{
+//     id: string,
+//     name: string,
+// }>
 
 
 export function CreateRoom(){
-    const { data, isLoading, } = useQuery({
-        queryKey: ['get-rooms'],
-        queryFn: async () => {
-            const response = await fetch('http://localhost:3333/rooms');
-            const result: GetRoomsAPIResponse  = await response.json()
+    // const { data, isLoading, } = useQuery({
+    //     queryKey: ['get-rooms'],
+    //     queryFn: async () => {
+    //         const response = await fetch('http://localhost:3333/rooms');
+    //         const result: GetRoomsAPIResponse  = await response.json()
 
-            return result
-        },
-    })
+    //         return result
+    //     },
+    // })
 
 
     return (
-        <>
-            {isLoading && <p>Carregando...</p>}
-            <div className="flex flex-col gap-1">
-                {data?.map(room => {
-                    return (
-                        <Link key={room.id} to={`/room/${room.id}`}>
-                            {room.name}
-                        </Link>
-                    )
-                })}
+        <div className="min-h-screen p-4">
+            <div className="mx-auto py-8 max-w-4xl">
+                <div className="grid gap-8 grid-cols-2 items-start gap-8"></div>
             </div>
-        </>
+        </div>
     )
 }
